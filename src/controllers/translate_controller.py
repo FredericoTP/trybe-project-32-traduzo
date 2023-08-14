4c583033030cbc1770bdf24c271a35cb72a90963
Frederico Tavares <fredericotp@hotmail.com>
2023-08-14 16:10:37 -0300

    Feat: Requisito 5

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
-# from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator
 from models.language_model import LanguageModel
 
 # from models.history_model import HistoryModel
@@ -13,10 +13,14 @@ translate_controller = Blueprint("translate_controller", __name__)
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
     languages = LanguageModel.list_dicts()
-    text_to_translate = ""
-    translate_from = "pt"
-    translate_to = "en"
-    translated = ""
+    text_to_translate = request.form.get("text-to-translate") or ""
+    translate_from = request.form.get("translate-from") or "pt"
+    translate_to = request.form.get("translate-to") or "en"
+    print(translate_to)
+    translator_google = GoogleTranslator(
+        source="auto", target=translate_to
+    ).translate(text_to_translate)
+    translated = translator_google or ""
     return render_template(
         "index.html",
         languages=languages,
